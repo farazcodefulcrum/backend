0db16573773825c5cd111b0b6140e30d6b28cdb1
farazsabir <faraz.sabir@codefulcrum.com>
2022-09-21 16:39:15 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6612860bed14c59cd5446210c75de337'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e19bb88f-a4be-4004-b9db-0652e59d6251'
+};
